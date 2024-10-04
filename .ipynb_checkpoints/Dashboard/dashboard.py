4462f32c800b16b443973a0b03a8e9a4a44bed3a
uray03 <urayhafizh@gmail.com>
2024-10-04 07:32:09 +0700

    update dashboard

--- .ipynb_checkpoints/Dashboard/dashboard.py
+++ .ipynb_checkpoints/Dashboard/dashboard.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from babel.numbers import format_currency
